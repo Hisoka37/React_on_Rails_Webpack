8830158ffc5aa5f5c1582092860deab585832698
Walid Kabboussa <walidkabou@gmail.com>
2023-07-18 13:08:54 +0100

    Setup and Fix Linters

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,7 +1,7 @@
 class Api::GreetingsController < ApplicationController
-    def hello
-      random_id = rand(1..5)
-      @greeting = Greeting.find(random_id)
-      render json: @greeting
-    end
+  def hello
+    random_id = rand(1..5)
+    @greeting = Greeting.find(random_id)
+    render json: @greeting
   end
+end
